51fe1ea67524c6d3332967c85c2afd367ab993b2
Christer <christerforsgren91@gmail.com>
2021-06-17 12:35:59 +0200

    Add axios call to viaplay url, map the series

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SerieList from './components/SerieList'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <SerieList />
+    </>
   )
 }
 
--- src/components/SerieList.jsx
+++ src/components/SerieList.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const SerieList = () => {
   const { series, setSeries } = useState([])
 
-  
+  const getSeries = async () => {
+    let response = await axios.get('https://content.viaplay.se/pc-se/serier/samtliga')
+    setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
+  }
 
+  useEffect(() => {
+    getSeries()
+  }, [])
+
+    let listOfSeries = series.map((serie) => {
+      return <SerieCard serie={serie} />
+    })
   return (
     <div data-cy='series-container'>
+      {series}
     </div>
   )
 }
