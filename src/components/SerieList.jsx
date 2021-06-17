c400a2ff649c17df77434986d6922ca80afb7a68
Christer <christerforsgren91@gmail.com>
2021-06-17 13:47:01 +0200

    remove serie card file, add header

--- src/components/SerieList.jsx
+++ src/components/SerieList.jsx
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SerieList = () => {
-  const { series, setSeries } = useState([])
+  const { series, setSeries } = useState([]);
 
   const getSeries = async () => {
-    let response = await axios.get('https://content.viaplay.se/pc-se/serier/samtliga')
-    setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
-  }
+    debugger;
+    let response = await axios.get(
+      'https://content.viaplay.se/pc-se/serier/samtliga'
+    );
+    setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']);
+  };
 
   useEffect(() => {
-    getSeries()
-  }, [])
+    getSeries();
+  }, []);
 
-    let listOfSeries = series.map((serie) => {
-      return <SerieCard serie={serie} />
-    })
-  return (
-    <div data-cy='series-container'>
-      {listOfSeries}
-    </div>
-  )
-}
+  let listOfSeries = series.map((serie) => {
+    return (
+      <div>
+        <img key={serie.id} src={serie.content.images.boxart.url} alt="series-list" />
+      </div>
+    );
+  });
+  return <div data-cy="series-container">{listOfSeries}</div>;
+};
 
-export default SerieList
+export default SerieList;
