bb5d260dc15d87602c3d9643abaf65b8181fc18f
Christer <christerforsgren91@gmail.com>
2021-06-17 12:05:49 +0200

    Add axios

---
+++ src/components/SerieList.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const SerieList = () => {
+  const { series, setSeries } = useState([])
+
+  
+
+  return (
+    <div data-cy='series-container'>
+    </div>
+  )
+}
+
+export default SerieList
